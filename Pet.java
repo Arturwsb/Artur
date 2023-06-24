9ab3afff0ccd427881fec9e058faa4a6056b358f
Arturwsb <137415243+Arturwsb@users.noreply.github.com>
2023-06-24 09:14:27 +0200

    Dodanie komentarzu w linii 4

--- Pet.java
+++ Pet.java
@@ -1,7 +1,7 @@
 public abstract class Pet {
     String name;
     int age;
-
+.// Proszę dodać komenatrz 
     public Pet(String name, int age) {
         this.name = name;
         this.age = age;
